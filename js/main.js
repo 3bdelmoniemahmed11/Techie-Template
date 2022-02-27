04dbdec00af9e9e4a48fd26fbdb16c9e97f42b8c
abdelmoniem ahmed <3bdelmoniemahmed11@gmail.com>
2022-02-27 03:43:44 +0200

    first section

---
+++ js/main.js
@@ -0,0 +1,36 @@
+//function to select with class or id
+const select=(element,all=false)=>{
+    element=element.trim();
+    if(all)
+    {
+        return[...document.querySelectorAll(element)];
+    }else
+    {
+        return document.querySelector(element);
+    }
+}
+
+//Easy on scroll event listener 
+
+const onscroll=(element,listner)=>
+{
+    element.addEventListener("scroll",listner)
+}
+//toggle header-scrolled class to header 
+
+let selectheader=select("#header");
+if(selectheader)
+{
+    const headerScrolled=()=>
+    {
+        if (window.scrollY>100)
+        {
+            selectheader.classList.add("header-scrolled");
+        }else
+        {
+            selectheader.classList.remove("header-scrolled");
+        }
+    }
+    window.addEventListener('load',headerScrolled);
+    onscroll(document,headerScrolled);
+}
