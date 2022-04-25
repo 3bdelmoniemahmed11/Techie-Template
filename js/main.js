b991d840d7e92f246872667c60591cf5936e7189
abdelmoniem ahmed <3bdelmoniemahmed11@gmail.com>
2022-04-25 18:27:51 +0200

    six-section

--- js/main.js
+++ js/main.js
@@ -34,3 +34,32 @@ if(selectheader)
     window.addEventListener('load',headerScrolled);
     onscroll(document,headerScrolled);
 }
+/*Testimonials slider*/
+new Swiper('.swiper', {
+   speed: 600,
+   loop:true,
+   autoplay:{
+       delay:5000,
+       disableOnInteraction:false
+   },
+   slidesPerView:'auto',
+    pagination:
+    {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+        320: {
+          slidesPerView: 1,
+          spaceBetween: 40
+        },
+  
+        1200: {
+          slidesPerView: 3,
+          spaceBetween: 40
+        }
+    }
+}
+  
+   );
