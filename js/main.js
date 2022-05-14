e4229a143299df39e9b65faf1c36588dee718418
abdelmoniem ahmed <3bdelmoniemahmed11@gmail.com>
2022-05-14 21:06:09 +0200

    portfolio-section-completed

--- js/main.js
+++ js/main.js
@@ -16,6 +16,24 @@ const onscroll=(element,listner)=>
 {
     element.addEventListener("scroll",listner)
 }
+  /**
+   * Easy event listener function
+   */
+
+  const on=(type,el,listner,all=false)=>
+  {
+      let selectel=select(el,all);
+      if(selectel)
+      {
+          if(all)
+          {
+              selectel.forEach(e=>e.addEventListener(type,listner));
+          }else
+          {
+              selectel.addEventListener(type,listner);
+          }
+      }
+  }
 //toggle header-scrolled class to header 
 
 let selectheader=select("#header");
@@ -34,6 +52,25 @@ if(selectheader)
     window.addEventListener('load',headerScrolled);
     onscroll(document,headerScrolled);
 }
+
+let navbarlinks=select(".navbar .scrollto",true);
+const navbarlinksActive=()=>{
+    let position=window.scrollY+200;
+    navbarlinks.forEach(navbarlink => {
+        if(!navbarlink.hash) return;
+        let section=select(navbarlink.hash)
+        if(position>=section.offsetTop &&position<=section.offsetTop+section.offsetHeight)
+        {
+            navbarlink.classList.add("active");
+        }else
+        {
+            navbarlink.classList.remove("active");
+        }
+    });
+}
+window.addEventListener("load",navbarlinksActive);
+onscroll(document,navbarlinksActive);
+
 /*Testimonials slider*/
 new Swiper('.swiper', {
    speed: 600,
@@ -61,5 +98,36 @@ new Swiper('.swiper', {
         }
     }
 }
-  
    );
+window.addEventListener("load",()=>{
+let portfolioContainer=select(".portfolio-container");
+if(portfolioContainer)
+{
+    let portfolioIsotope=new Isotope(".portfolio-container",{
+        itemSelector:'.portfolio-item'
+    })
+    let portfolioFilters=select(".portfolio-filters li",true);
+
+    on('click',".portfolio-filters li",function(e)
+    {
+        e.preventDefault();
+        portfolioFilters.forEach(function(el)
+        {
+            el.classList.remove("filter-active");
+        })
+        this.classList.add("filter-active");
+        portfolioIsotope.arrange({
+            filter:this.getAttribute("data-filter")
+        })
+
+
+    },true)
+}
+
+});
+/**
+   * Initiate portfolio lightbox 
+   */
+  const portfolioLightbox=GLightbox({
+      selector:".portfolio-lightbox"
+  })
